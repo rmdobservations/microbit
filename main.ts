54d69ac202b16ec3a1d3b1816293d9baaaab5e8a
Rose Dlhopolsky <rmdobservations@users.noreply.github.com>
2021-10-28 16:14:34 +0200

    commentaar toegevoegd

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 let appel = 0
+/**
+ * Elke klik op A gaat appel omhoog bij 1. Op B is reset tot 0.
+ */
 input.onButtonPressed(Button.A, function () {
     appel += 1
+    basic.showNumber(appel)
+})
+input.onButtonPressed(Button.B, function () {
+    appel = 0
     basic.showNumber(appel)
 })
 basic.forever(function () {
